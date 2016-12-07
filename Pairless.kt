a3008e8a0587898c56c83e9626cafc60c557cfde
Victor Bolinches <vicboma1@users.noreply.github.com>
2016-12-07 16:11:40 +0100

    Create Pairless.kt

---
+++ Pairless.kt
@@ -0,0 +1,36 @@
+/*
+ * Think of a perfect world where everybody has a soulmate.
+ * Now, the real world is imperfect: there is exactly one number in the array
+ * that does not have a pair. A pair is an element with the same value.
+ * For example in this array:
+ *   1, 2, 1, 2
+ * every number has a pair, but in this one:
+ *   1, 1, 1
+ * one of the ones is lonely.
+ *
+ * Your task is to implement the findPairless() function so that it finds the
+ * lonely number and returns it.
+ *
+ * A hint: there's a solution that looks at each element only once and uses no
+ * data structures like collections or trees.
+ */
+package pairless
+
+fun findPairless(a: IntArray): Int {
+    // Write your solution here
+    return 0
+}
+
+/** Solution 1
+
+INPUT   OUTPUT
+A   B   A XOR B
+0   0      0      OK
+0   1      1
+1   0      1
+1   1      0      OK  */
+
+
+fun findPairless(a: IntArray): Int {
+    return a.reduce { a, b -> a xor b }
+}
