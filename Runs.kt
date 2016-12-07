aa6d752a5b5eaa6519731a85db3159c2ca7763c4
Victor Bolinches <vicboma1@users.noreply.github.com>
2016-12-07 16:09:50 +0100

    Create Runs.kt

---
+++ Runs.kt
@@ -0,0 +1,37 @@
+/*
+ * Any array may be viewed as a number of "runs" of equal numbers.
+ * For example, the following array has two runs:
+ *   1, 1, 1, 2, 2
+ * Three 1's in a row form the first run, and two 2's form the second.
+ * This array has two runs of length one:
+ *   3, 4
+ * And this one has five runs:
+ *   1, 0, 1, 1, 1, 2, 0, 0, 0, 0, 0, 0, 0
+ * Your task is to implement the runs() function so that it returns the number
+ * of runs in the given array.
+ */
+package runs
+
+fun runs(a: IntArray): Int {
+    // Write your solution here
+    return 0
+}
+
+
+// Solution 1 
+
+fun runs(a: IntArray): Int {
+    var res = 0;
+    if (a.size == res)
+        return res
+
+    var base = a[res]
+    a.forEach({
+        if(it != base){
+            res ++
+            base = it
+        }
+    })
+
+    return (++res);   
+}
